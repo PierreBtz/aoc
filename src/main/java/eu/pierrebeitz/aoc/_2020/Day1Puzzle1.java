bdc0421862a6c2f15b752e148e4e5140a8885376
Pierre Beitz <pibeitz@gmail.com>
2020-12-02 23:08:48 +0100

    basic solution for day 1

---
+++ src/main/java/eu/pierrebeitz/aoc/_2020/Day1Puzzle1.java
@@ -0,0 +1,40 @@
+package eu.pierrebeitz.aoc._2020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class Day1Puzzle1 {
+    private static final int TARGET = 2020;
+
+    public static void main(String[] args) throws IOException {
+        //let's go simple, given the number of inputs n2 is acceptable
+        try (var reader =
+                   new BufferedReader(
+                         new InputStreamReader(Day1Puzzle1.class.getResourceAsStream("/eu/pierrebeitz/aoc/_2020/input-day1.txt")))) {
+            var expenses = reader.lines()
+                  .map(Integer::valueOf)
+                  .collect(Collectors.toList());
+
+            var numberOne = Integer.MIN_VALUE;
+            var numberTwo = Integer.MIN_VALUE;
+            for (Integer value1 : expenses) {
+                for (Integer value2 : expenses) {
+                    if (value1 + value2 == TARGET) {
+                        System.err.printf("Number 1 is %d, Number 2 is %d%n", value1, value2);
+                        numberOne = value1;
+                        numberTwo = value2;
+                        break;
+                    }
+                    if (numberOne != Integer.MIN_VALUE) {
+                        break;
+                    }
+                }
+            }
+            // puzzle is guaranteed to have a solution, no error handling
+            System.out.println(numberOne * numberTwo);
+        }
+    }
+
+}
---
+++ src/main/java/eu/pierrebeitz/aoc/_2020/Day1Puzzle2.java
@@ -0,0 +1,44 @@
+package eu.pierrebeitz.aoc._2020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class Day1Puzzle2 {
+    private static final int TARGET = 2020;
+
+    public static void main(String[] args) throws IOException {
+        //let's go simple, given the number of inputs n3 is acceptable
+        try (var reader =
+                   new BufferedReader(
+                         new InputStreamReader(Day1Puzzle2.class.getResourceAsStream("/eu/pierrebeitz/_2020/input-day1.txt")))) {
+            var expenses = reader.lines()
+                  .map(Integer::valueOf)
+                  .collect(Collectors.toList());
+
+            var numberOne = Integer.MIN_VALUE;
+            var numberTwo = Integer.MIN_VALUE;
+            var numberThree = Integer.MIN_VALUE;
+            for (Integer value1 : expenses) {
+                for (Integer value2 : expenses) {
+                    for (Integer value3 : expenses) {
+                        if (value1 + value2 + value3 == TARGET) {
+                            System.err.printf("Number 1 is %d, Number 2 is %d, Number 3 is %d%n", value1, value2, value3);
+                            numberOne = value1;
+                            numberTwo = value2;
+                            numberThree = value3;
+                            break;
+                        }
+                        if (numberOne != Integer.MIN_VALUE) {
+                            break;
+                        }
+                    }
+                }
+            }
+            // puzzle is guaranteed to have a solution, no error handling
+            System.out.println(numberOne * numberTwo * numberThree);
+        }
+    }
+
+}
