d28777dde1b388cd8255b3c99bd7508879b904ef
Pierre Beitz <pibeitz@gmail.com>
2020-12-05 21:07:47 +0100

    Day 5 puzzle 2

---
+++ src/main/java/eu/pierrebeitz/aoc/_2020/Day5Puzzle2.java
@@ -0,0 +1,33 @@
+package eu.pierrebeitz.aoc._2020;
+
+import eu.pierrebeitz.aoc._2020.Day5Puzzle1.Seat;
+import eu.pierrebeitz.aoc.utils.AocUtils;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.summarizingInt;
+
+public class Day5Puzzle2 {
+
+    public static void main(String[] args) throws IOException {
+        try (var reader = AocUtils.loadInputForDay(5)) {
+            var ids = reader.lines()
+                  .map(Seat::fromString)
+                  .map(Seat::getId)
+                  .collect(Collectors.toList());
+            var stats = ids.stream()
+                  .collect(summarizingInt(Integer::intValue));
+            var max = stats.getMax();
+            var min = stats.getMin();
+            var allIds = IntStream.range(min, max + 1).boxed();
+            var missingIds = allIds
+                  .filter(id -> !ids.contains(id))
+                  .collect(Collectors.toList());
+            assert missingIds.size() == 1 : "We did not get the expected number of id";
+            System.out.println(missingIds.get(0));
+        }
+    }
+
+}
