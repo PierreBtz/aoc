745a79c0f35c7b3b2ad08bc1953a226511a35059
Pierre Beitz <pibeitz@gmail.com>
2020-12-06 15:53:05 +0100

    Day 6 puzzle 2

--- src/main/java/eu/pierrebeitz/aoc/_2020/Day6Puzzle1.java
+++ src/main/java/eu/pierrebeitz/aoc/_2020/Day6Puzzle1.java
@@ -3,30 +3,32 @@ package eu.pierrebeitz.aoc._2020;
 import eu.pierrebeitz.aoc.utils.AocUtils;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Day6Puzzle1 {
 
     public static void main(String[] args) throws IOException {
+        computeCount(new Puzzle1GroupBuilder());
+    }
+
+    static void computeCount(Function<String, Group> groupBuilder) throws IOException {
         try (var reader = AocUtils.loadInputForDay(6)) {
-            var count = AocUtils.readByGroupAndMapToObject(reader, Group::new).stream()
+            var count = AocUtils.readByGroupAndMapToObject(reader, groupBuilder).stream()
                   .peek(System.err::println)
                   .map(Group::getQuestionCount)
                   .reduce(0, Integer::sum);
             System.out.println(count);
-
         }
     }
 
     static class Group {
         private final Set<Character> allAnswers;
 
-        private Group(String str) {
-            allAnswers = str.chars()
-                  .mapToObj(c -> (char) c)
-                  .filter(c -> c != '\n')
-                  .collect(Collectors.toSet());
+        Group(Set<Character> allAnswers) {
+            this.allAnswers = Collections.unmodifiableSet(allAnswers);
         }
 
         int getQuestionCount() {
@@ -38,6 +40,17 @@ public class Day6Puzzle1 {
             return "Group{" +
                   "allAnswers=" + allAnswers +
                   '}';
+        }
+    }
+
+    private static class Puzzle1GroupBuilder implements Function<String, Group> {
+
+        @Override
+        public Group apply(String s) {
+            return new Group(s.chars()
+                  .mapToObj(c -> (char) c)
+                  .filter(c -> c != '\n')
+                  .collect(Collectors.toSet()));
         }
     }
 }
