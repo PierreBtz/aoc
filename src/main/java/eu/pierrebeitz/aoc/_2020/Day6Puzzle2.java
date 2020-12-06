6113306c6cd2c10ce8f9e6e5e01355ec9e3819a9
Pierre Beitz <pibeitz@gmail.com>
2020-12-06 15:53:05 +0100

    Day 6 puzzle 2

---
+++ src/main/java/eu/pierrebeitz/aoc/_2020/Day6Puzzle2.java
@@ -0,0 +1,35 @@
+package eu.pierrebeitz.aoc._2020;
+
+import eu.pierrebeitz.aoc._2020.Day6Puzzle1.Group;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static eu.pierrebeitz.aoc._2020.Day6Puzzle1.computeCount;
+
+public class Day6Puzzle2 {
+
+    public static void main(String[] args) throws IOException {
+        computeCount(new Puzzle2GroupBuilder());
+    }
+
+    static class Puzzle2GroupBuilder implements Function<String, Group> {
+
+        @Override
+        public Group apply(String s) {
+            var numberOfPeople = s.lines().count();
+            var questionSet = s.chars()
+                  .mapToObj(c -> (char) c)
+                  .filter(c -> c != '\n')
+                  .collect(Collectors.groupingBy(c -> c, Collectors.counting()))
+                  .entrySet()
+                  .stream()
+                  .filter(entry -> entry.getValue() == numberOfPeople)
+                  .map(Map.Entry::getKey)
+                  .collect(Collectors.toSet());
+            return new Group(questionSet);
+        }
+    }
+}
